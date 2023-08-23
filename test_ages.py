2a94f4c84474011606f997a09f3b03b32f7f7885
ggjiuw <herezoom1231@gmail.com>
2023-08-23 16:51:27 +0300

    renamed every function

--- test_ages.py
+++ test_ages.py
@@ -1,28 +1,28 @@
 from ages import check_age
 
 
-def test_check_age():
+def test_invalid_age():
     expected_result = 'Passed invalid age'
     age = -1
     actual_result = check_age(age)
     assert actual_result == expected_result
 
 
-def test_check_age():
+def test_child_age():
     expected_result = 'You are still a child'
     age = 9
     actual_result = check_age(age)
     assert actual_result == expected_result
 
 
-def test_check_age():
+def test_working_age():
     expected_result = 'You need to find a job'
     age = 25
     actual_result = check_age(age)
     assert actual_result == expected_result
 
 
-def test_check_age():
+def test_retirement_age():
     expected_result = 'Happy retirement!'
     age = 70
     actual_result = check_age(age)
