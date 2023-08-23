fe8e328775890ddd933c0a718d635355537f1f69
ggjiuw <herezoom1231@gmail.com>
2023-08-23 16:50:32 +0300

    removed input and print , leaving the function

--- ages.py
+++ ages.py
@@ -1,20 +1,13 @@
 
 
-def check_age(user_age_input):
+def check_age(user_age_input: int) -> str:
     if user_age_input <= 0:
-        check_age_output = 'Passed invalid age'
+        return 'Passed invalid age'
     elif user_age_input >= 1:
         if user_age_input >= 19:
             if user_age_input >= 66:
-                check_age_output = 'Happy retirement!'
+                return 'Happy retirement!'
             elif user_age_input <= 65:
-                check_age_output = 'You need to find a job'
+                return 'You need to find a job'
         elif user_age_input <= 18:
-            check_age_output = 'You are still a child'
-
-    return check_age_output
-
-
-user_age = int(input('Enter your age > '))
-
-print(check_age(user_age))
+            return 'You are still a child'
